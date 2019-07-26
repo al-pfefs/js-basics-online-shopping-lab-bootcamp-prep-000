7d29413e66ef05fc9d98ae7a02bb84d21317c44a
Ali Pfeffer <alpfefs@gmail.com>
2019-07-26 18:19:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function removeFromCart(item) {
   for (i = 0; i < cart.length; i++) {
     items.push(cart[i].itemName)
   }
-  return items 
+  console.log(items)
   
   if items.includes(item) {
     itemIndex = items.indexOf(item)
