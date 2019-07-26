f7ee8f5018c9ccfef84f5906c04dd566709c76d0
Ali Pfeffer <alpfefs@gmail.com>
2019-07-26 15:01:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,12 +20,18 @@ function viewCart() {
   // write your code here
   var string = 'In your cart, you have '
   for (var i = 0; i <= cart.length; i++) {
-    if (cart.length === 1) {
+    if (cart.length === 0) {
        string = 'Your shopping cart is empty.'
        } 
       else if (cart.length === 1) {
         string+= `${Object.keys(cart[i])} at $${Object.values(cart[i])}.`
       } 
+      
+  }
+  return string
+}
+
+      
       else if (i >= 1 && cart.length > 1) {
       string += `${Object.keys(cart[i])} at $${Object.values(cart[i])}, `
     }
