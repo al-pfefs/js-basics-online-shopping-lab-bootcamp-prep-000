8a50d19cce6d65e86650656ae401155c4883ad72
Ali Pfeffer <alpfefs@gmail.com>
2019-07-26 16:54:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ function removeFromCart(item) {
   if items.includes(item) {
     itemIndex = items.indexOf(item)
     cart.splice(i,1)
+    return cart
   }
   else {
     return "That item is not in your cart."
