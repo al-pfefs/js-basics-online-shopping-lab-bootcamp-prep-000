2d2d58d2569fe18da7f1397802795610c43bbd5a
Ali Pfeffer <alpfefs@gmail.com>
2019-07-26 15:06:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
        string = 'Your shopping cart is empty.'
        } 
       else if (cart.length === 1 && i === 0) {
-        string+= `${Object.keys(cart[0])} at $${Object.values(cart[0])}.`
+        string += `${Object.keys(cart[0])} at $${Object.values(cart[0])}.`
       } 
       else if (i >= 1 && cart.length >= 1) {
         string += `${Object.keys(cart[i])} at $${Object.values(cart[i])}, `
