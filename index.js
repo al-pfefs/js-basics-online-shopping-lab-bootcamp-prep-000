897446f10494f3f0fdca75582509df8f04ab33c0
Ali Pfeffer <alpfefs@gmail.com>
2019-07-26 18:32:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ if (cardNumber === undefined) {
   return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    total = total()
+   var total = total()
     cart.splice(0)
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
