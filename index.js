a7cd331113434d51094f1a335b6c7081ac817d6a
Ali Pfeffer <alpfefs@gmail.com>
2019-07-26 15:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
     else if (cart.length === 1 && i === 0) {
       string += `${Object.keys(cart[0])} at $${Object.values(cart[0])}.`
         } 
-    else {
+    else if (i < cart.length-1) {
       string += `${Object.keys(cart[i])} at $${Object.values(cart[i])}, `
         }
   }
